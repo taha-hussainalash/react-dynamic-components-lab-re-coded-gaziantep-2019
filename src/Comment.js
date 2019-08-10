e8d9a414a5c5f615b81a68d127c92bfe10c97d80
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-10 20:10:23 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,2 +1,11 @@
 // add Comment component here
 import React,{component} from "react";
+
+
+class Comment extends React.Component {
+  	    render() {
+        return (
+            <div>{this.props.commentText}</div>	            <div className = {"comment"}>{this.props.commentText}</div>
+        )	        )
+    }	    }
+}	}
